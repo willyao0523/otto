7bbfa142aacf49b95c04a7f6caae04c660828416
wyao <willjarvin@gmail.com>
2023-07-11 22:53:42 +0800

    feat: init the framework core and main func

--- framework/core.go
+++ framework/core.go
@@ -1,1 +1,16 @@
 package framework
+
+import "net/http"
+
+// framework core
+type Core struct {}
+
+// init framework core
+func NewCore() *Core {
+	return &Core{}
+}
+
+// handlers
+func (c *Core) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+
+}
--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package otto
+
+import (
+	"net/http"
+	"otto/framework"
+)
+
+func main() {
+	server := &http.Server{
+		Handler: framework.NewCore(),
+		Addr: ":8080",
+	}
+	server.ListenAndServe()
+}
